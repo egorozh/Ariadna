4a720b58f6b9db23658830ad1dc37bc5cf6ad9fa
Egor Zheludkov <zheludkov.egor@mail.ru>
2021-10-13 14:43:09 +0300

    Add controls libs

---
+++ src/Controls/Ariadna.AvalonDockTheme/AriadnaTheme.cs
@@ -0,0 +1,10 @@
+﻿using AvalonDock.Themes;
+using System;
+
+namespace Ariadna.AvalonDockTheme;
+
+public class AriadnaTheme : Theme
+{
+    public override Uri GetResourceUri() =>
+        new ("/Ariadna.AvalonDockTheme;component/Resources/Theme.xaml", UriKind.Relative);
+}
