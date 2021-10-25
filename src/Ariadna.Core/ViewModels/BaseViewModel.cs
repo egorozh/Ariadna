273bed28e51cf039984eea86af369ba958374087
Egor Zheludkov <zheludkov.egor@mail.ru>
2021-10-25 14:04:33 +0300

    Add core lib

---
+++ src/Ariadna.Core/ViewModels/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Ariadna.Core;
+
+public class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public event EventHandler<ExPropertyChangedEventArgs>? ExPropertyChanged;
+
+    public void OnPropertyChanged(string propertyName, object before, object after)
+    {
+        PropertyChanged?.Invoke(this, new ExPropertyChangedEventArgs(propertyName, before, after));
+        ExPropertyChanged?.Invoke(this, new ExPropertyChangedEventArgs(propertyName, before, after));
+    }
+}
---
+++ src/Ariadna.Core/ViewModels/ExPropertyChangedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Ariadna.Core;
+
+public class ExPropertyChangedEventArgs : PropertyChangedEventArgs
+{
+    public object? OldValue { get; }
+    public object? NewValue { get; }
+
+    public ExPropertyChangedEventArgs(string propertyName, object? oldValue, object? newValue)
+        : base(propertyName)
+    {
+        OldValue = oldValue;
+        NewValue = newValue;
+    }
+}
